d6e833ec55bc3181b7ba1cdfd7db6ba7d7ba7599
eyeballss <kkvv1111@gmail.com>
2019-02-07 07:28:35 -0500

    fix directory name and Workload_IO.java

---
+++ HDFS_code/Workload_IO.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Workload_IO {
+        public static void main(String[] args) throws Exception {
+                Configuration conf = new Configuration();
+
+                Job job = Job.getInstance(conf, "Worklaod_IO");
+                job.setJarByClass(Workload_IO.class);
+                job.setMapOutputKeyClass(Text.class);
+                job.setMapOutputValueClass(Text.class);
+                job.setOutputKeyClass(NullWritable.class);
+                job.setOutputValueClass(Text.class);
+                job.setMapperClass(Map.class);
+                job.setReducerClass(Reduce.class);
+                job.setInputFormatClass(TextInputFormat.class);
+                job.setOutputFormatClass(TextOutputFormat.class);
+
+                FileInputFormat.addInputPath(job, new Path(args[0]));
+                FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+                job.waitForCompletion(true);
+
+        }
+
+        public static class Map extends Mapper<Object, Text, Text, Text> {
+                public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+                        context.write(new Text(key.toString()), value);
+                }
+        }
+
+        public static class Reduce extends Reducer<Text, Text, NullWritable, Text> {
+                public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+                        Text val = values.iterator().next();
+                        if (val != null) {
+                                context.write(NullWritable.get(), val);
+                        }
+                }
+        }
+}
