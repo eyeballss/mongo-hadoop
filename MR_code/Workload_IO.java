6e8eaee4d144e976a03a87cda3a2e675b82261fd
eyeballss <kkvv1111@gmail.com>
2019-02-07 05:48:58 -0500

    fix Workload_IO.java for the result

--- MR_code/Workload_IO.java
+++ MR_code/Workload_IO.java
@@ -13,35 +13,40 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Workload_IO {
-        public static void main(String[] args) throws Exception {
-                Configuration conf = new Configuration();
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
 
-                Job job = Job.getInstance(conf, "Worklaod_IO");
-                job.setJarByClass(Workload_IO.class);
-                job.setOutputKeyClass(Text.class);
-                job.setOutputValueClass(NullWritable.class);
-                job.setMapperClass(Map.class);
-                job.setReducerClass(Reduce.class);
-                job.setInputFormatClass(TextInputFormat.class);
-                job.setOutputFormatClass(TextOutputFormat.class);
+		Job job = Job.getInstance(conf, "Worklaod_IO");
+		job.setJarByClass(Workload_IO.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-                FileInputFormat.addInputPath(job, new Path(args[0]));
-                FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-                job.waitForCompletion(true);
+		job.waitForCompletion(true);
 
-        }
+	}
 
-        public static class Map extends Mapper<Object, Text, Text, NullWritable> {
-                public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-                        context.write(value, NullWritable.get());
-                }
-        }
+	public static class Map extends Mapper<Object, Text, Text, Text> {
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			context.write(new Text(key.toString()), value);
+		}
+	}
 
-        public static class Reduce extends Reducer<Text, Text, Text, NullWritable> {
-                public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-                        context.write(key, NullWritable.get());
-                }
-        }
+	public static class Reduce extends Reducer<Text, Text, Text, NullWritable> {
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			Text val = values.iterator().next();
+			if (val != null) {
+				context.write(val, NullWritable.get());
+			}
+		}
+	}
 }
 
