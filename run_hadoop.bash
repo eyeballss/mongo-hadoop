6fc6723b094ee0445065f74b0cf522c61de491ab
eyeballss <kkvv1111@gmail.com>
2019-02-07 03:22:54 -0500

    fix run_hadoop.bash

--- run_hadoop.bash
+++ run_hadoop.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# $1 : java code name without ".java"
+# $1 : java code name
 # $2 : data path in HDFS
 
 if [[ -z "$2" ]]
@@ -8,6 +8,8 @@ then
         exit 0
 fi
 
+sudo rm -r -f classes
+mkdir classes
 javac -classpath `hadoop classpath` -d classes $1.java
 jar -cvf $1.jar -C classes/ .
 hadoop fs -rm -r -f /$2.out
