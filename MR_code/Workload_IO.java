952ed32c345df10465bf729b7d5f9099edee7d05
eyeballss <kkvv1111@gmail.com>
2019-02-07 03:23:08 -0500

    fix Workload_IO.java

--- MR_code/Workload_IO.java
+++ MR_code/Workload_IO.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,31 +13,35 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Workload_IO {
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
+        public static void main(String[] args) throws Exception {
+                Configuration conf = new Configuration();
 
-		Job job = Job.getInstance(conf, "Worklaod_IO");
-		job.setJarByClass(Workload_IO.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
-		job.setMapperClass(Map.class);
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+                Job job = Job.getInstance(conf, "Worklaod_IO");
+                job.setJarByClass(Workload_IO.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(NullWritable.class);
+                job.setMapperClass(Map.class);
+                job.setReducerClass(Reduce.class);
+                job.setInputFormatClass(TextInputFormat.class);
+                job.setOutputFormatClass(TextOutputFormat.class);
 
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+                FileInputFormat.addInputPath(job, new Path(args[0]));
+                FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		job.waitForCompletion(true);
+                job.waitForCompletion(true);
 
-	}
+        }
 
-	public static class Map extends Mapper<Object, Text, Text, Text> {
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-		}
-	}
+        public static class Map extends Mapper<Object, Text, Text, NullWritable> {
+                public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+                        context.write(value, NullWritable.get());
+                }
+        }
 
-	public static class Reduce extends Reducer<Text, Text, Text, Text> {
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-		}
-	}
+        public static class Reduce extends Reducer<Text, Text, Text, NullWritable> {
+                public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+                        context.write(key, NullWritable.get());
+                }
+        }
 }
+
