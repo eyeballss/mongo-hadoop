c52d2d484758051e36afb27ae30a288b3ce234a9
eyeballss <kkvv1111@gmail.com>
2019-03-08 02:12:15 -0500

    update PageRank algorithm to use mongo query at pretreatment

--- HDFS_code/PageRank.java
+++ HDFS_code/PageRank.java
@@ -1,15 +1,7 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -22,27 +14,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class PageRank {
 
-	private static final Text STATIC_KEY = new Text("statickKey");
-	private static final String ALL_COUNT = "allCount";
 	private static final double DAMPING_FACTOR = 0.85;
 	private static final String SEPARATOR = "\t";
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-
-		Job job = job1Setting(conf, args, "", ".all.count");
-		job.waitForCompletion(true);
-
-		try {
-			Path pt = new Path("hdfs:" + args[0] + ".all.count/part-r-00000");// Location of file in HDFS
-			FileSystem fs = FileSystem.get(new Configuration());
-			BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(pt)));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				conf.set(ALL_COUNT, line);
-			}
-		} catch (Exception e) {
-		}
 
 		Job job2 = job2Setting(conf, args, "", ".0");
 		job2.waitForCompletion(true);
@@ -59,35 +35,6 @@ public class PageRank {
 
 	}
 
-	public static class AllNodesCountMap extends Mapper<Text, Text, Text, Text> {
-		HashSet<String> set = new HashSet<>();
-
-		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-			set.add(key.toString());
-//			set.add(value.toString());
-		}
-
-		protected void cleanup(Mapper<Text, Text, Text, Text>.Context context)
-				throws IOException, InterruptedException {
-			Iterator<String> itr = set.iterator();
-			while (itr.hasNext()) {
-				context.write(new Text(STATIC_KEY), new Text(itr.next()));
-			}
-		}
-
-	}
-
-	public static class AllNodesReduce extends Reducer<Text, Text, NullWritable, Text> {
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			HashSet<String> set = new HashSet<>();
-			for (Text temp : values) {
-				String value = temp.toString();
-				set.add(value);
-			}
-			context.write(NullWritable.get(), new Text(String.valueOf(set.size())));
-		}
-	}
-
 	public static class PretreatmentMap extends Mapper<Text, Text, Text, Text> {
 		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 			context.write(key, value);
@@ -98,8 +45,7 @@ public class PageRank {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
 			StringBuilder sb = new StringBuilder();
-			String allCount = context.getConfiguration().get(ALL_COUNT);
-			double initRank = 1d / (Double.parseDouble(allCount));
+			double initRank = 1d;
 			sb.append(initRank);
 			for (Text value : values) {
 				sb.append(SEPARATOR);
@@ -111,20 +57,21 @@ public class PageRank {
 	}
 
 	public static class PageRankMap extends Mapper<Text, Text, Text, Text> {
-		private Text outKey = new Text();
-		private Text outValue = new Text();
 
-		@Override
 		protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 			context.write(key, value); // original input data
 
 			String[] parts = StringUtils.splitPreserveAllTokens(value.toString(), SEPARATOR);
-			if (parts.length > 2) {
-				double outboundPageRank = Double.parseDouble(parts[0]) / (parts.length - 1);
-				for (int i = 1; i < parts.length; i++) {
+			if (parts.length >= 2) {
+				int qnsah = parts.length - 1;
+				if (qnsah == 0)
+					return;
+
+				double outboundPageRank = Double.parseDouble(parts[0]) / (double) qnsah;
+				for (int i = 1; i <= qnsah; i++) {
 					String outbound = parts[i];
-					outKey.set(outbound);
-					outValue.set(String.valueOf(outboundPageRank));
+					Text outKey = new Text(outbound);
+					Text outValue = new Text(String.valueOf(outboundPageRank).trim());
 					context.write(outKey, outValue);
 				}
 			}
@@ -135,20 +82,15 @@ public class PageRank {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
 			double totalPageRank = 0;
-			int allCount = 0;
-			ArrayList<String> outbounds = null;
+			String outbounds = null;
 
 			for (Text value : values) {
-				String[] parts = StringUtils.splitPreserveAllTokens(value.toString(), SEPARATOR);
+				String[] parts = StringUtils.splitPreserveAllTokens(value.toString().trim(), SEPARATOR);
 
 				if (parts.length < 2) { // rank value
 					totalPageRank += Double.parseDouble(parts[0]);
 				} else { // original input data
-					outbounds = new ArrayList<String>();
-					allCount = Integer.parseInt(context.getConfiguration().get(ALL_COUNT));
-					for (int i = 1; i < parts.length; i++) {
-						outbounds.add(parts[i]);
-					}
+					outbounds = value.toString();
 				}
 			}
 
@@ -157,17 +99,19 @@ public class PageRank {
 				return;
 			}
 
-			double dampingFactor = ((1d - DAMPING_FACTOR) / (double) allCount);
+			double dampingFactor = (1d - DAMPING_FACTOR);
 			double newPageRank = dampingFactor + (DAMPING_FACTOR) * (totalPageRank);
+
+			String[] result = outbounds.split(SEPARATOR);
 
 			StringBuilder sb = new StringBuilder();
 			sb.append(newPageRank);
-			for (String outbound : outbounds) {
-				sb.append(SEPARATOR + outbound);
+			for (int i = 1; i < result.length; i++) {
+				sb.append(SEPARATOR + result[i]);
 			}
 
 			context.write(key, new Text(sb.toString()));
-
+			totalPageRank = 0;
 		}
 	}
 
@@ -178,27 +122,6 @@ public class PageRank {
 
 			context.write(key, new Text(values[0]));
 		}
-	}
-
-	private static Job job1Setting(Configuration conf, String[] args, String from, String to) throws IOException {
-		Job job = Job.getInstance(conf, ALL_COUNT);
-		job.setJarByClass(PageRank.class);
-
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setOutputKeyClass(NullWritable.class);
-		job.setOutputValueClass(Text.class);
-
-		job.setNumReduceTasks(1);
-
-		job.setMapperClass(AllNodesCountMap.class);
-		job.setReducerClass(AllNodesReduce.class);
-
-		job.setInputFormatClass(KeyValueTextInputFormat.class);
-
-		FileInputFormat.addInputPath(job, new Path(args[0] + from));
-		FileOutputFormat.setOutputPath(job, new Path(args[0] + to));
-		return job;
 	}
 
 	private static Job job2Setting(Configuration conf, String[] args, String from, String to) throws IOException {
@@ -251,4 +174,3 @@ public class PageRank {
 	}
 
 }
-
--- Mongo_code/PageRank.java
+++ Mongo_code/PageRank.java
@@ -1,15 +1,10 @@
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,11 +14,13 @@ import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.bson.BasicBSONObject;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.AggregateIterable;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.hadoop.MongoInputFormat;
@@ -34,45 +31,46 @@ import com.mongodb.hadoop.util.MongoConfigUtil;
 
 public class PageRank {
 
-	private static final Text STATIC_KEY = new Text("statickKey");
-	private static final String ALL_COUNT = "allCount";
 	private static final double DAMPING_FACTOR = 0.85;
 	private static final String SEPARATOR = "\t";
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 
-		MongoConfigUtil.setInputURI(conf, "mongodb://mongo-router/dataset.data");
+		MongoConfigUtil.setInputURI(conf, "mongodb://mongo-router/dataset.result");
+		MongoConfigUtil.setOutputURI(conf, "mongodb://mongo-router/dataset.data.out");
 		MongoConfigUtil.setSplitterClass(conf, ShardMongoSplitter.class);
 
 		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://mongo-router:27017"));
-		MongoDatabase database = mongoClient.getDatabase("dataset");
-		MongoCollection<Document> coll = database.getCollection("data");
 
-		AggregateIterable<Document> output = coll
-				.aggregate(
-						Arrays.asList(
-								new Document("$group",
-										new Document("_id", "null").append("fromCount",
-												new Document("$addToSet", "$from"))),
-								new Document("$project", new Document("size", new Document("$size", "$fromCount")))));
+		long start = System.currentTimeMillis();
 
-		String allCount = null;
-		try {
-			for (Document dbObject : output) {
-				allCount = String.valueOf(dbObject.get("size"));
+		if (true) {
+			try {
+
+				MongoDatabase database = mongoClient.getDatabase("dataset");
+				MongoCollection<Document> collection = database.getCollection("data");
+
+				Block<Document> processBlock = new Block<Document>() {
+					@Override
+					public void apply(final Document document) {
+					}
+				};
+				List<? extends Bson> pipeline = Arrays.asList(
+						new Document().append("$group",
+								new Document().append("_id", "$from").append("init", new Document().append("$min", "1"))
+										.append("to", new Document().append("$push", "$to"))),
+						new Document().append("$out", "result"));
+				collection.aggregate(pipeline).allowDiskUse(true).forEach(processBlock);
+			} catch (MongoException e) {
+				// handle MongoDB exception
+e.printStackTrace();
+return;
 			}
-		} catch (ClassCastException e) {
-			System.err.println(e.toString());
-			return;
-		} finally {
-			if (allCount == null) {
-				System.err.println("no size");
-				return;
-			} else {
-				conf.set(ALL_COUNT, allCount);
-			}
-		}
+		} // if
+
+		long end = System.currentTimeMillis(); 
+		System.out.println( "query time : " + ( end - start )/1000.0 +" sec");
 
 		Job job2 = job2Setting(conf, args, "", ".0");
 		job2.waitForCompletion(true);
@@ -89,72 +87,43 @@ public class PageRank {
 
 	}
 
-	public static class AllNodesCountMap extends Mapper<Text, Text, Text, Text> {
-		HashSet<String> set = new HashSet<>();
-
-		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-			set.add(key.toString());
-			set.add(value.toString());
-		}
-
-		protected void cleanup(Mapper<Text, Text, Text, Text>.Context context)
-				throws IOException, InterruptedException {
-			Iterator<String> itr = set.iterator();
-			while (itr.hasNext()) {
-				context.write(new Text(STATIC_KEY), new Text(itr.next()));
-			}
-		}
-
-	}
-
-	public static class AllNodesReduce extends Reducer<Text, Text, NullWritable, Text> {
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			HashSet<String> set = new HashSet<>();
-			for (Text temp : values) {
-				String value = temp.toString();
-				set.add(value);
-			}
-			context.write(NullWritable.get(), new Text(String.valueOf(set.size())));
-		}
-	}
-
 	public static class PretreatmentMap extends Mapper<Integer, BasicDBObject, Text, Text> {
 		public void map(Integer key, BasicDBObject value, Context context) throws IOException, InterruptedException {
-			context.write(new Text(value.getString("from")), new Text(value.getString("to")));
-		}
-	}
 
-	public static class PretreatmentReduce extends Reducer<Text, Text, Text, Text> {
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			Text keyText = new Text(String.valueOf(key));
+
+			String to = value.getString("to");
+			String tos[] = to.subSequence(1, to.length() - 1).toString().split(",");
 
 			StringBuilder sb = new StringBuilder();
-			String allCount = context.getConfiguration().get(ALL_COUNT);
-			double initRank = 1d / (Double.parseDouble(allCount));
-			sb.append(initRank);
-			for (Text value : values) {
+			sb.append(value.getString("init"));
+			for (String t : tos) {
 				sb.append(SEPARATOR);
-				sb.append(value.toString());
+				sb.append(t);
 			}
 
-			context.write(key, new Text(sb.toString()));
+			Text valueText = new Text(sb.toString());
+
+			context.write(keyText, valueText);
 		}
 	}
 
 	public static class PageRankMap extends Mapper<Text, Text, Text, Text> {
-		private Text outKey = new Text();
-		private Text outValue = new Text();
 
-		@Override
 		protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
 			context.write(key, value); // original input data
 
 			String[] parts = StringUtils.splitPreserveAllTokens(value.toString(), SEPARATOR);
-			if (parts.length > 2) {
-				double outboundPageRank = Double.parseDouble(parts[0]) / (parts.length - 1);
-				for (int i = 1; i < parts.length; i++) {
+			if (parts.length >= 2) {
+				int qnsah = parts.length - 1;
+				if (qnsah == 0)
+					return;
+
+				double outboundPageRank = Double.parseDouble(parts[0]) / (double) qnsah;
+				for (int i = 1; i <= qnsah; i++) {
 					String outbound = parts[i];
-					outKey.set(outbound);
-					outValue.set(String.valueOf(outboundPageRank));
+					Text outKey = new Text(outbound);
+					Text outValue = new Text(String.valueOf(outboundPageRank).trim());
 					context.write(outKey, outValue);
 				}
 			}
@@ -165,20 +134,15 @@ public class PageRank {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
 			double totalPageRank = 0;
-			int allCount = 0;
-			ArrayList<String> outbounds = null;
+			String outbounds = null;
 
 			for (Text value : values) {
-				String[] parts = StringUtils.splitPreserveAllTokens(value.toString(), SEPARATOR);
+				String[] parts = StringUtils.splitPreserveAllTokens(value.toString().trim(), SEPARATOR);
 
 				if (parts.length < 2) { // rank value
 					totalPageRank += Double.parseDouble(parts[0]);
 				} else { // original input data
-					outbounds = new ArrayList<String>();
-					allCount = Integer.parseInt(context.getConfiguration().get(ALL_COUNT));
-					for (int i = 1; i < parts.length; i++) {
-						outbounds.add(parts[i]);
-					}
+					outbounds = value.toString();
 				}
 			}
 
@@ -187,17 +151,19 @@ public class PageRank {
 				return;
 			}
 
-			double dampingFactor = ((1d - DAMPING_FACTOR) / (double) allCount);
+			double dampingFactor = (1d - DAMPING_FACTOR);
 			double newPageRank = dampingFactor + (DAMPING_FACTOR) * (totalPageRank);
+
+			String[] result = outbounds.split(SEPARATOR);
 
 			StringBuilder sb = new StringBuilder();
 			sb.append(newPageRank);
-			for (String outbound : outbounds) {
-				sb.append(SEPARATOR + outbound);
+			for (int i = 1; i < result.length; i++) {
+				sb.append(SEPARATOR + result[i]);
 			}
 
 			context.write(key, new Text(sb.toString()));
-
+			totalPageRank = 0;
 		}
 	}
 
@@ -213,41 +179,20 @@ public class PageRank {
 
 			context.write(key, result);
 		}
-	}
 
-	private static Job job1Setting(Configuration conf, String[] args, String from, String to) throws IOException {
-		Job job = Job.getInstance(conf, ALL_COUNT);
-		job.setJarByClass(PageRank.class);
-
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(Text.class);
-		job.setOutputKeyClass(NullWritable.class);
-		job.setOutputValueClass(Text.class);
-
-		job.setNumReduceTasks(1);
-
-		job.setMapperClass(AllNodesCountMap.class);
-		job.setReducerClass(AllNodesReduce.class);
-
-		job.setInputFormatClass(KeyValueTextInputFormat.class);
-
-		FileInputFormat.addInputPath(job, new Path(args[0] + from));
-		FileOutputFormat.setOutputPath(job, new Path(args[0] + to));
-		return job;
 	}
 
 	private static Job job2Setting(Configuration conf, String[] args, String from, String to) throws IOException {
 		Job job = Job.getInstance(conf, "pretreatment");
 		job.setJarByClass(PageRank.class);
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setNumReduceTasks(0);
 
 		job.setMapperClass(PretreatmentMap.class);
-		job.setReducerClass(PretreatmentReduce.class);
 		job.setInputFormatClass(MongoInputFormat.class);
 
-//		FileInputFormat.addInputPath(job, new Path(args[0] + from));
 		FileOutputFormat.setOutputPath(job, new Path(args[0] + to));
 		return job;
 	}
@@ -271,7 +216,6 @@ public class PageRank {
 
 	private static Job jobCleanUpSetting(Configuration conf, String[] args, String from, String to) throws IOException {
 
-		MongoConfigUtil.setOutputURI(conf, "mongodb://mongo-router/dataset.data.out");
 		Job job = Job.getInstance(conf, "PageRankFinishing");
 		job.setJarByClass(PageRank.class);
 		job.setNumReduceTasks(0);
@@ -283,9 +227,8 @@ public class PageRank {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(BSONWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0] + from));
-//		FileOutputFormat.setOutputPath(job, new Path(args[0] + to));
 
 		return job;
 	}
-
 }
+
