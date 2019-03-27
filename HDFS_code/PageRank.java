07f9b8749ec1fd9440684cd2c030f4e864910d77
eyeballss <kkvv1111@gmail.com>
2019-03-27 10:52:22 -0400

    fix variable names in PageRank code

--- HDFS_code/PageRank.java
+++ HDFS_code/PageRank.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -20,18 +19,21 @@ public class PageRank {
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 
-		Job job2 = job2Setting(conf, args, "", ".0");
-		job2.waitForCompletion(true);
+		//first job for data pretreatment
+		Job pretreatmentJob = pretreatmentJobSetting(conf, args, "", ".0");
+		pretreatmentJob.waitForCompletion(true);
 
+		//second job for updating page rank
 		int i = 0;
-		Job job3;
+		Job rankingUpdateJob;
 		for (i = 1; i <= 1; i++) {
-			job3 = jobRankSetting(conf, args, "." + (i - 1), "." + i);
-			job3.waitForCompletion(true);
+			rankingUpdateJob = rankingUpdateJobSetting(conf, args, "." + (i - 1), "." + i);
+			rankingUpdateJob.waitForCompletion(true);
 		}
 
-		Job job4 = jobCleanUpSetting(conf, args, "." + (i - 1), ".out");
-		job4.waitForCompletion(true);
+		//third(last) job for cleaning up the result to show 'page' and 'rank' only.
+		Job cleaningUpJob = cleaningUpJobSetting(conf, args, "." + (i - 1), ".out");
+		System.exit(cleaningUpJob.waitForCompletion(true) ? 0 : 1);
 
 	}
 
@@ -63,16 +65,16 @@ public class PageRank {
 
 			String[] parts = StringUtils.splitPreserveAllTokens(value.toString(), SEPARATOR);
 			if (parts.length >= 2) {
-				int qnsah = parts.length - 1;
-				if (qnsah == 0)
+				int numOfTo = parts.length - 1;
+				if (numOfTo == 0)
 					return;
 
-				double outboundPageRank = Double.parseDouble(parts[0]) / (double) qnsah;
-				for (int i = 1; i <= qnsah; i++) {
-					String outbound = parts[i];
-					Text outKey = new Text(outbound);
-					Text outValue = new Text(String.valueOf(outboundPageRank).trim());
-					context.write(outKey, outValue);
+				double rank = Double.parseDouble(parts[0]) / (double) numOfTo;
+				for (int i = 1; i <= numOfTo; i++) {
+					String to = parts[i];
+					Text mapKey = new Text(to);
+					Text mapValue = new Text(String.valueOf(rank).trim());
+					context.write(mapKey, mapValue);
 				}
 			}
 		}
@@ -81,58 +83,55 @@ public class PageRank {
 	public static class PageRankReduce extends Reducer<Text, Text, Text, Text> {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-			double totalPageRank = 0;
-			String outbounds = null;
+			double totalRank = 0;
+			String rankAndSetOfTo = null;
 
 			for (Text value : values) {
 				String[] parts = StringUtils.splitPreserveAllTokens(value.toString().trim(), SEPARATOR);
 
 				if (parts.length < 2) { // rank value
-					totalPageRank += Double.parseDouble(parts[0]);
+					totalRank += Double.parseDouble(parts[0]);
 				} else { // original input data
-					outbounds = value.toString();
+					rankAndSetOfTo = value.toString();
 				}
 			}
 
-			// this node doesn't have any inbound links.
-			if (outbounds == null) {
+			// This node doesn't have any inbound links.
+			if (rankAndSetOfTo == null) {
 				return;
 			}
 
 			double dampingFactor = (1d - DAMPING_FACTOR);
-			double newPageRank = dampingFactor + (DAMPING_FACTOR) * (totalPageRank);
+			double newRank = dampingFactor + (DAMPING_FACTOR) * (totalRank);
 
-			String[] result = outbounds.split(SEPARATOR);
+			//Find first index of SEPARATOR
+			int index = rankAndSetOfTo.indexOf(SEPARATOR);
+			//replace the old rank value before SEPARATOR to the new rank
+			rankAndSetOfTo = newRank+rankAndSetOfTo.substring(index);
 
-			StringBuilder sb = new StringBuilder();
-			sb.append(newPageRank);
-			for (int i = 1; i < result.length; i++) {
-				sb.append(SEPARATOR + result[i]);
-			}
-
-			context.write(key, new Text(sb.toString()));
-			totalPageRank = 0;
+			context.write(key, new Text(rankAndSetOfTo));
 		}
 	}
 
-	private static class CleanUpMap extends Mapper<Text, Text, Text, Text> {
+	private static class CleaningUpMap extends Mapper<Text, Text, Text, Text> {
 		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-			String line = value.toString();
-			String values[] = line.split(SEPARATOR);
-
-			context.write(key, new Text(values[0]));
+			String rankAndTo = value.toString();
+			int index = rankAndTo.indexOf(SEPARATOR);
+			String rank = rankAndTo.substring(0,index);
+			context.write(key, new Text(rank));
 		}
 	}
 
-	private static Job job2Setting(Configuration conf, String[] args, String from, String to) throws IOException {
+	private static Job pretreatmentJobSetting(Configuration conf, String[] args, String from, String to) throws IOException {
 		Job job = Job.getInstance(conf, "pretreatment");
 		job.setJarByClass(PageRank.class);
+
+		job.setMapperClass(PretreatmentMap.class);
+		job.setReducerClass(PretreatmentReduce.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 
-		job.setMapperClass(PretreatmentMap.class);
-		job.setReducerClass(PretreatmentReduce.class);
 		job.setInputFormatClass(KeyValueTextInputFormat.class);
 
 		FileInputFormat.addInputPath(job, new Path(args[0] + from));
@@ -140,9 +139,10 @@ public class PageRank {
 		return job;
 	}
 
-	private static Job jobRankSetting(Configuration conf, String[] args, String from, String to) throws IOException {
-		Job job = Job.getInstance(conf, "page rank");
+	private static Job rankingUpdateJobSetting(Configuration conf, String[] args, String from, String to) throws IOException {
+		Job job = Job.getInstance(conf, "updating page rank");
 		job.setJarByClass(PageRank.class);
+		
 		job.setMapperClass(PageRankMap.class);
 		job.setReducerClass(PageRankReduce.class);
 
@@ -157,16 +157,19 @@ public class PageRank {
 		return job;
 	}
 
-	private static Job jobCleanUpSetting(Configuration conf, String[] args, String from, String to) throws IOException {
-		Job job = Job.getInstance(conf, "PageRankFinishing");
+	private static Job cleaningUpJobSetting(Configuration conf, String[] args, String from, String to) throws IOException {
+		Job job = Job.getInstance(conf, "cleaning up");
 		job.setJarByClass(PageRank.class);
-		job.setNumReduceTasks(0);
-		job.setMapperClass(CleanUpMap.class);
+		
+		job.setMapperClass(CleaningUpMap.class);
 
+		job.setNumReduceTasks(0);
+		
 		job.setInputFormatClass(KeyValueTextInputFormat.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
+		
 		FileInputFormat.addInputPath(job, new Path(args[0] + from));
 		FileOutputFormat.setOutputPath(job, new Path(args[0] + to));
 
